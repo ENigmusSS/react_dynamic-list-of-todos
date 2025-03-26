e41108a19f20d25267d9068012fb74c3067ab84d
ENigmusSS <128493758+ENigmusSS@users.noreply.github.com>
2025-03-26 22:09:44 +0200

    ☼ution

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable max-len */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import 'bulma/css/bulma.css';
 import '@fortawesome/fontawesome-free/css/all.css';
 
@@ -7,8 +7,30 @@ import { TodoList } from './components/TodoList';
 import { TodoFilter } from './components/TodoFilter';
 import { TodoModal } from './components/TodoModal';
 import { Loader } from './components/Loader';
+import { Todo } from './types/Todo';
+import { getTodos } from './api';
 
 export const App: React.FC = () => {
+  const [shownTodos, setShownTodos] = useState<Todo[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isModalShown, setIsModalShown] = useState(false);
+  const [modalTodo, setModalTodo] = useState<Todo>();
+  const todosFromServer = React.useRef<Todo[]>([]);
+
+  const openModal = (todo: Todo) => {
+    setModalTodo(todo);
+    setIsModalShown(true);
+  };
+
+  useEffect(() => {
+    setIsLoading(true);
+    getTodos().then(todos => {
+      todosFromServer.current = todos;
+      setShownTodos(todosFromServer.current);
+      setIsLoading(false);
+    });
+  }, []);
+
   return (
     <>
       <div className="section">
@@ -17,18 +39,27 @@ export const App: React.FC = () => {
             <h1 className="title">Todos:</h1>
 
             <div className="block">
-              <TodoFilter />
+              <TodoFilter
+                todosToFilter={todosFromServer.current}
+                applyFilters={todos => setShownTodos(todos)}
+              />
             </div>
 
             <div className="block">
-              <Loader />
-              <TodoList />
+              {isLoading && <Loader />}
+              <TodoList todos={shownTodos} onTodoSelect={openModal} />
             </div>
           </div>
         </div>
       </div>
 
-      <TodoModal />
+      {isModalShown && (
+        <TodoModal
+          todo={modalTodo as Todo}
+          onModalClose={() => setIsModalShown(false)}
+          key={`modal${modalTodo?.id}`}
+        />
+      )}
     </>
   );
 };
